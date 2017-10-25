a7be9973922b9847a1b16216d04479aa5fe65122
Yossef Adi <adiyoss@devfair056.maas>
2017-10-25 12:00:35 -0700

    /

--- run_demo.py
+++ run_demo.py
@@ -25,9 +25,9 @@ def run_demo():
                         help='Input WAV file')
     parser.add_argument('--sample_rate_hz', default=8000, type=int,
                         help='Sample rate in Hz')
-    parser.add_argument('--fft_size', default=512, type=int,
+    parser.add_argument('--fft_size', default=1024, type=int,
                         help='FFT siz')
-    parser.add_argument('--iterations', default=300, type=int,
+    parser.add_argument('--iterations', default=1000, type=int,
                         help='Number of iterations to run')
     parser.add_argument('--enable_filter', action='store_true',
                         help='Apply a low-pass filter', default=True)
